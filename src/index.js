ec78293c1a6d5a6df011967249331f3aaa7749a7
emma-ezeilo <ezeiloemmanuel7@gmail.com>
2024-08-22 15:58:37 +0100

    actions, reducers, store and react-redux in redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import './containers/App.css'
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider, connect } from "react-redux"; // This is what connects redux and react together 
+import { createStore } from "redux"; // This is the module to create a store with redux 
+import { searchRobots } from './reducers';
 import 'tachyons'
 
+const store = createStore(searchRobots) // from documentation createStore() is legacy
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
--- src/reducers.js
+++ src/reducers.js
@@ -8,7 +8,7 @@ export const searchRobots = (state=initialState, action={})=>{
     // This is a reducer function
     switch (action.type) {
         case: CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, searchField: action.payload);
+            return Object.assign({}, state, searchField: action.payload);  // You can also use this syntax { ... state, searchField: action.payload);}
         default:
             return state;
     }
